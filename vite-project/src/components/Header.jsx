5e626bc7caefc00b058bb02798c16c78edac67a4
Emily Caton <caton.emily@yahoo.com>
2023-11-12 23:21:15 -0600

    validate name, email, and message entries in contact form, update styling for and center form, change class to className

--- vite-project/src/components/Header.jsx
+++ vite-project/src/components/Header.jsx
@@ -3,32 +3,32 @@ import React from "react";
 function Header({setCurrentPage}) {
   return (
     <header>
-      <nav class="bg-blue-300">
-  <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-    <div class="relative flex h-16 items-center justify-between">
-      <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-        {/* <div class="flex flex-shrink-0 items-center">
-          <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company"/>
+      <nav className="bg-blue-300">
+  <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+    <div className="relative flex h-16 items-center justify-between">
+      <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+        {/* <div className="flex flex-shrink-0 items-center">
+          <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company"/>
         </div> */}
-        <div class="hidden sm:ml-6 sm:block">
-          <div class="flex space-x-4">
+        <div className="hidden sm:ml-6 sm:block">
+          <div className="flex space-x-4">
            
-            <a href="#" class="bg-violet-400 text-sky-950 hover:text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page"
+            <a href="#" className="bg-violet-400 text-sky-950 hover:text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page"
             onClick={() => {
                 setCurrentPage("About")
               }}
             >About Me</a>
-            <a href="#" class="text-sky-950 hover:bg-violet-400 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+            <a href="#" className="text-sky-950 hover:bg-violet-400 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
             onClick={() => {
                 setCurrentPage("Portfolio")
               }}
             >Portfolio</a>
-            <a href="#" class="text-sky-950 hover:bg-violet-400 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+            <a href="#" className="text-sky-950 hover:bg-violet-400 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
             onClick={() => {
                 setCurrentPage("Contact")
               }}
             >Contact</a>
-            <a href="#" class="text-sky-950 hover:bg-violet-400 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Resume</a>
+            <a href="#" className="text-sky-950 hover:bg-violet-400 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Resume</a>
           </div>
         </div>
       </div>
@@ -36,13 +36,13 @@ function Header({setCurrentPage}) {
   </div>
 
 
-  <div class="sm:hidden" id="mobile-menu">
-    <div class="space-y-1 px-2 pb-3 pt-2">
+  <div className="sm:hidden" id="mobile-menu">
+    <div className="space-y-1 px-2 pb-3 pt-2">
      
-      <a href="#" class="bg-violet-400 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">About Me</a>
-      <a href="#" class="text-gray-300 hover:bg-violet-400 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Portfolio</a>
-      <a href="#" class="text-gray-300 hover:bg-violet-400 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Contact</a>
-      <a href="#" class="text-gray-300 hover:bg-violet-400 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Resume</a>
+      <a href="#" className="bg-violet-400 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">About Me</a>
+      <a href="#" className="text-gray-300 hover:bg-violet-400 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Portfolio</a>
+      <a href="#" className="text-gray-300 hover:bg-violet-400 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Contact</a>
+      <a href="#" className="text-gray-300 hover:bg-violet-400 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Resume</a>
     </div>
   </div>
 </nav>
--- vite-project/src/pages/Contact.jsx
+++ vite-project/src/pages/Contact.jsx
@@ -10,11 +10,27 @@ const validateEmail = (email) => {
 };
 
 function Contact() {
+    const [name, setName] = useState("");
     const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+    const [nameValid, setNameValid] = useState(true);
     const [emailValid, setEmailValid] = useState(true);
+    const [messageValid, setMessageValid] = useState(true);
+
+    function handleNameChange(event) {
+        setName(event.target.value);
+    }
 
     function handleEmailChange(event) {
         setEmail(event.target.value)
+    }
+
+    function handleMessageBoxChange(event) {
+        setMessage(event.target.value);
+    }
+
+    function checkName() {
+        setNameValid(name !== "");
     }
 
     function checkEmail() {
@@ -25,47 +41,75 @@ function Contact() {
         }
     }
 
+    function checkMessage() {
+        setMessageValid(message !== "");
+    }
+
     return (
         <>
-            <form class="container px-4">
-                <div class="space-y-12">
-                    <div class="border-b border-gray-900/10 pb-12">
-                        <h2 class="text-base font-semibold leading-7 text-gray-900">Contact</h2>
-                        <p class="mt-1 text-sm leading-6 text-gray-600">Leave a comment or question for Emily</p>
+         <div className="flex items-center justify-center h-screen">
+            <form className="bg-pink-300 container px-4">
+                <div className="space-y-12">
+                    <div className="border-b border-gray-900/10 pb-12">
+                        <h2 className="text-base font-semibold leading-7 text-gray-900">Contact</h2>
+                        <p className="mt-1 text-sm leading-6 text-gray-600">Leave a comment or question for Emily</p>
 
-                        <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                            <div class="sm:col-span-4">
-                                <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Name</label>
-                                <div class="mt-2">
-                                    <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                        <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm"></span>
-                                        <input type="text" name="username" id="username" autocomplete="username" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="your name" />
-                                    </div>
+                        <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                            <div className="sm:col-span-4">
+                                <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">Name</label>
+                                <div className="mt-2">
+                                    <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm"></span>
+                                        <input
+                                            type="text"
+                                            name="username"
+                                            id="username"
+                                            autoComplete="username"
+                                            className="block flex-1 border-0 py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                            placeholder="Your name"
+                                            value={name}
+                                            onChange={handleNameChange}
+                                            onBlur={checkName}
+                                        />
+                                   
+                                    {!nameValid && <p className="mt-2 text-sm text-red-600" id="name-error">Name is required</p>}
                                 </div>
                             </div>
 
-                            <div class="col-span-full">
-                                <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
-                                <div class="mt-2">
-                                    <input id="email" name="email" type="email" autocomplete="email" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            <div className="col-span-full">
+                                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+                                <div className="mt-2">
+                                    <input
+                                        id="email"
+                                        name="email"
+                                        type="email"
+                                        autoComplete="email"
+                                        className="block w-full rounded-md border-0 py-1.5 pl-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                        placeholder="your.email@example.com"
                                         value={email}
                                         onChange={handleEmailChange}
                                         onBlur={checkEmail}
                                     />
-                                    {
-                                        emailValid ?
-                                            "" :
-                                            <p class="mt-2 text-sm text-red-600" id="email-error">Email is required</p>}
+                                    {!emailValid && <p className="mt-2 text-sm text-red-600" id="email-error">Valid email is required</p>}
                                 </div>
                             </div>
 
-                            <div class="col-span-full">
-                                <label for="about" class="block text-sm font-medium leading-6 text-gray-900">Message</label>
-                                <div class="mt-2">
-                                    <textarea id="about" name="about" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                            <div className="col-span-full">
+                                <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">Message</label>
+                                <div className="mt-2">
+                                    <textarea
+                                        id="about"
+                                        name="about"
+                                        rows="3"
+                                        className="block w-full rounded-md border-0 py-1.5 pl-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                        placeholder="Your message..."
+                                        value={message}
+                                        onChange={handleMessageBoxChange}
+                                        onBlur={checkMessage}
+                                    />
+                                    {!messageValid && <p className="mt-2 text-sm text-red-600" id="message-error">Message is required</p>}
                                 </div>
                                 <button type="submit"
-                                    className="mt-2 p-2 float-right text-white   
+                                    className="mt-2 p-2 float-right text-white
                                    rounded-lg bg-emerald-400 hover:scale-105">
                                     Send message
                                 </button>
@@ -74,9 +118,9 @@ function Contact() {
                     </div>
                 </div>
             </form>
-
+        </div>
         </>
-    )
+    );
 }
 
 export default Contact;
