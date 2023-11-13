68122b1ced78178a18be7d330c1efb89e899a56c
Emily Caton <caton.emily@yahoo.com>
2023-11-13 15:36:44 -0600

    add to footer, install fontawesome and use icons to link socials

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -5,7 +5,11 @@ import Portfolio from './pages/Portfolio'
 import './App.css'
 import Contact from './pages/Contact'
 import Resume from './pages/Resume'
-
+import Footer from './components/Footer'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+library.add(fab);
 function App() {
   // const currentPage = "About";
   const [currentPage, setCurrentPage] = useState("About")
@@ -13,8 +17,7 @@ function App() {
   function render () {
     if(currentPage == "About") {
       return (
-        <About>
-        </About>
+        <About></About>
       )
     }
     else if(currentPage == "Portfolio") {
@@ -33,9 +36,7 @@ function App() {
       )
     }
   }
-
-
-  return (
+return (
     <>
       <Header
         setCurrentPage={setCurrentPage}
@@ -43,6 +44,9 @@ function App() {
       <main>
         {render()}
       </main>
+      <div className="flex justify-center p-4">
+      <Footer></Footer>
+      </div>
     </>
   )
 }
--- vite-project/src/components/Footer.jsx
+++ vite-project/src/components/Footer.jsx
@@ -1,10 +1,33 @@
-
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Footer() {
   return (
-    <footer>
-      <p>List Socials</p>
+    <footer className="bg-blue-300 text-white p-4">
+      <p className="mb-2">Connect with me:</p>
+      <div className="flex space-x-4">
+        <a
+          href="https://github.com/mLek10"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={['fab', 'github']} size="2x" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/emily-caton-48124a27a/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={['fab', 'linkedin']} size="2x" />
+        </a>
+        <a
+          href="https://stackoverflow.com/users/21502627/mlek10"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={['fab', 'stack-overflow']} size="2x" />
+        </a>
+      </div>
     </footer>
   );
 }
